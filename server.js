8584a8772456fa06790cc916c3445804b9e1a106
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-02-14 18:41:04 -0300

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 // import route middleware
 const authRouter = require("./routes/auth");
